f7111183bfa63510d6f5a52f0f4622ba65173cd7
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-06 13:37:15 +0000

    re-try template

--- src/main/java/org/identifiers/cloud/libapi/services/ResourceRecommenderService.java
+++ src/main/java/org/identifiers/cloud/libapi/services/ResourceRecommenderService.java
@@ -2,6 +2,9 @@ package org.identifiers.cloud.libapi.services;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.retry.backoff.FixedBackOffPolicy;
+import org.springframework.retry.policy.SimpleRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
 
 /**
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
@@ -15,4 +18,18 @@ public class ResourceRecommenderService {
     public static final int WS_REQUEST_RETRY_BACK_OFF_PERIOD = 1500; // 1.5 seconds
 
     private static final Logger logger = LoggerFactory.getLogger(ResourceRecommenderService.class);
+
+    // Re-try pattern, externalize this later if needed
+    private static final RetryTemplate retryTemplate;
+    static {
+        SimpleRetryPolicy retryPolicy = new SimpleRetryPolicy();
+        retryPolicy.setMaxAttempts(WS_REQUEST_RETRY_MAX_ATTEMPTS);
+
+        FixedBackOffPolicy backOffPolicy = new FixedBackOffPolicy();
+        backOffPolicy.setBackOffPeriod(WS_REQUEST_RETRY_BACK_OFF_PERIOD);
+
+        retryTemplate = new RetryTemplate();
+        retryTemplate.setRetryPolicy(retryPolicy);
+        retryTemplate.setBackOffPolicy(backOffPolicy);
+    }
 }
