bb5e95ad38c1d0aaf90a4a8b8ffb82376449b8c4
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-06 12:06:55 +0000

    ServiceRequestRecommend

---
+++ src/main/java/org/identifiers/cloud/libapi/models/ResourceRecommender/ServiceRequestRecommend.java
@@ -0,0 +1,13 @@
+package org.identifiers.cloud.libapi.models.ResourceRecommender;
+
+import org.identifiers.cloud.libapi.models.ServiceRequest;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: libapi
+ * Package: org.identifiers.cloud.libapi.models.ResourceRecommender
+ * Timestamp: 2018-03-06 12:04
+ * ---
+ */
+public class ServiceRequestRecommend extends ServiceRequest<RecommendRequestPayload> {
+}
