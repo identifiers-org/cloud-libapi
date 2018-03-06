2502d5929fd67b88f1029b5c266a2f58616e3e9a
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-06 11:35:52 +0000

    ResourceRecommenderService, placeholder

---
+++ src/main/java/org/identifiers/cloud/libapi/services/ResourceRecommenderService.java
@@ -0,0 +1,11 @@
+package org.identifiers.cloud.libapi.services;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: libapi
+ * Package: org.identifiers.cloud.libapi.services
+ * Timestamp: 2018-03-06 11:35
+ * ---
+ */
+public class ResourceRecommenderService {
+}
