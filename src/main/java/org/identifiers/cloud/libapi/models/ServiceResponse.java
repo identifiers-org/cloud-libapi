91df00207b1b2dec03fe4e6801260857df2e081c
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-06 12:10:43 +0000

    ServiceResponse skeleton

--- src/main/java/org/identifiers/cloud/libapi/models/ServiceResponse.java
+++ src/main/java/org/identifiers/cloud/libapi/models/ServiceResponse.java
@@ -1,5 +1,10 @@
 package org.identifiers.cloud.libapi.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.springframework.http.HttpStatus;
+
+import java.io.Serializable;
+
 /**
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * Project: libapi
@@ -7,5 +12,10 @@ package org.identifiers.cloud.libapi.models;
  * Timestamp: 2018-03-06 11:32
  * ---
  */
-public class ServiceResponse {
+@JsonIgnoreProperties(ignoreUnknown = true, value = {"httpStatus"})
+public class ServiceResponse<T> implements Serializable {
+    private String errorMessage;
+    private HttpStatus httpStatus = HttpStatus.OK;
+    // payload
+    private T payload;
 }
