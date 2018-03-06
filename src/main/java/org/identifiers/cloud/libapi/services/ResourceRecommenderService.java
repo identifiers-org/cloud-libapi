912016154b4764da1b84695f18fbbc05f3cc64e7
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-06 13:36:19 +0000

    get a logger

--- src/main/java/org/identifiers/cloud/libapi/services/ResourceRecommenderService.java
+++ src/main/java/org/identifiers/cloud/libapi/services/ResourceRecommenderService.java
@@ -1,5 +1,8 @@
 package org.identifiers.cloud.libapi.services;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * Project: libapi
@@ -10,4 +13,6 @@ package org.identifiers.cloud.libapi.services;
 public class ResourceRecommenderService {
     public static final int WS_REQUEST_RETRY_MAX_ATTEMPTS = 12;
     public static final int WS_REQUEST_RETRY_BACK_OFF_PERIOD = 1500; // 1.5 seconds
+
+    private static final Logger logger = LoggerFactory.getLogger(ResourceRecommenderService.class);
 }
