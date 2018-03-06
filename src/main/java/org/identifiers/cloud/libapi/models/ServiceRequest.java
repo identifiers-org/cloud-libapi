ddfdf84585d4c659db427545d4e4fadedd8fc292
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-06 11:55:38 +0000

    getters and setters

--- src/main/java/org/identifiers/cloud/libapi/models/ServiceRequest.java
+++ src/main/java/org/identifiers/cloud/libapi/models/ServiceRequest.java
@@ -15,4 +15,22 @@ import java.io.Serializable;
 public abstract class ServiceRequest<T> implements Serializable {
     private String apiVersion;
     private T payload;
+
+    public String getApiVersion() {
+        return apiVersion;
+    }
+
+    public ServiceRequest setApiVersion(String apiVersion) {
+        this.apiVersion = apiVersion;
+        return this;
+    }
+
+    public T getPayload() {
+        return payload;
+    }
+
+    public ServiceRequest setPayload(T payload) {
+        this.payload = payload;
+        return this;
+    }
 }
