3bfc7ad564f593725a54492bb86714b5ba0da371
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-06 16:41:00 +0000

    resource recommender service unit test, placeholder

---
+++ src/test/java/org/identifiers/cloud/libapi/services/ResourceRecommenderServiceTest.java
@@ -0,0 +1,18 @@
+package org.identifiers.cloud.libapi.services;
+
+import org.junit.Test;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: libapi
+ * Package: org.identifiers.cloud.libapi.services
+ * Timestamp: 2018-03-06 16:36
+ * ---
+ */
+public class ResourceRecommenderServiceTest {
+
+    @Test
+    public void requestRecommendations() {
+        // TODO
+    }
+}
