20521fbf4f2bfaa7dff5a3367501c26109489612
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-06 11:37:36 +0000

    library configuration, placeholder

---
+++ src/main/java/org/identifiers/cloud/libapi/Configuration.java
@@ -0,0 +1,11 @@
+package org.identifiers.cloud.libapi;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: libapi
+ * Package: org.identifiers.cloud.libapi
+ * Timestamp: 2018-03-06 11:36
+ * ---
+ */
+public class Configuration {
+}
