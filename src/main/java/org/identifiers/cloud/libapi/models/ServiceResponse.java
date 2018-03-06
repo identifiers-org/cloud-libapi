cfdc9c0beca18daeee6f081b90c8bca4ac879f1c
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-06 11:32:18 +0000

    placeholder for service response

---
+++ src/main/java/org/identifiers/cloud/libapi/models/ServiceResponse.java
@@ -0,0 +1,11 @@
+package org.identifiers.cloud.libapi.models;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: libapi
+ * Package: org.identifiers.cloud.libapi.models
+ * Timestamp: 2018-03-06 11:32
+ * ---
+ */
+public class ServiceResponse {
+}
