510931df6a4ae628c804cd48f3c863d0cfb1c2fb
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-06 13:19:25 +0000

    API Factory, for API Web Services wrappers

---
+++ src/main/java/org/identifiers/cloud/libapi/ApiFactory.java
@@ -0,0 +1,11 @@
+package org.identifiers.cloud.libapi;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: libapi
+ * Package: org.identifiers.cloud.libapi
+ * Timestamp: 2018-03-06 13:18
+ * ---
+ */
+public class ApiFactory {
+}
