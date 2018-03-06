49db01ddba00119d2c7c6d05551e96e1b38abedb
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-06 13:21:09 +0000

    re-try patter parameters

--- src/main/java/org/identifiers/cloud/libapi/services/ResourceRecommenderService.java
+++ src/main/java/org/identifiers/cloud/libapi/services/ResourceRecommenderService.java
@@ -8,5 +8,6 @@ package org.identifiers.cloud.libapi.services;
  * ---
  */
 public class ResourceRecommenderService {
-
+    public static final int WS_REQUEST_RETRY_MAX_ATTEMPTS = 12;
+    public static final int WS_REQUEST_RETRY_BACK_OFF_PERIOD = 1500; // 1.5 seconds
 }
