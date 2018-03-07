6532d2baed581c5bff049be0a1fec6e484409253
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-07 03:59:54 +0000

    working unit test

--- src/test/java/org/identifiers/cloud/libapi/services/ResourceRecommenderServiceTest.java
+++ src/test/java/org/identifiers/cloud/libapi/services/ResourceRecommenderServiceTest.java
@@ -1,6 +1,21 @@
 package org.identifiers.cloud.libapi.services;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.identifiers.cloud.libapi.ApiServicesFactory;
+import org.identifiers.cloud.libapi.models.ResourceRecommender.ResolvedResource;
+import org.identifiers.cloud.libapi.models.ResourceRecommender.ServiceResponseRecommend;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 /**
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
@@ -10,6 +25,7 @@ import org.junit.Test;
  * ---
  */
 public class ResourceRecommenderServiceTest {
+    private static Logger logger = LoggerFactory.getLogger(ResourceRecommenderServiceTest.class);
 
     @Test
     public void requestRecommendations() {
@@ -17,5 +33,30 @@ public class ResourceRecommenderServiceTest {
         // There is no testing data for this iteration of the library, so this unit test is design as a simple API
         // compliance test, where we check that the client can effectively connect the web service and pull a response
         // back from it
+        List<ResolvedResource> resolvedResources = IntStream.range(0, 9).parallel().mapToObj(index -> {
+            return new ResolvedResource()
+                    .setAccessURL(String.format("http://resolved.resource/%d", index))
+                    .setId(Integer.toString(index));
+        }).collect(Collectors.toList());
+        // Set an official one
+        resolvedResources.get(0).setOfficial(true);
+        // Get the service wrapper
+        ResourceRecommenderService service = ApiServicesFactory
+                .getResourceRecommenderService("localhost", "8083");
+        // Call the service
+        ServiceResponseRecommend response = service.requestRecommendations(resolvedResources);
+        assertThat("Response from service is OK",
+                response.getHttpStatus() == HttpStatus.OK,
+                is(true));
+        assertThat("We get the same number of resources back",
+                response.getPayload().getResourceRecommendations().size() == resolvedResources.size(),
+                is(true));
+        // Just for debugging purposes, serialized response into the logs
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            logger.info("Test request recommendation, response from the service:\n{}", mapper.writeValueAsString(response));
+        } catch (JsonProcessingException e) {
+            // Ignore
+        }
     }
 }
