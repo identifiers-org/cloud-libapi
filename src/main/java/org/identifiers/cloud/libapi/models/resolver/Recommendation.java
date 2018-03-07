5bef1b9b6ec4cef26f77743111731e754427fa3c
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-07 07:53:29 +0000

    refactor

--- src/main/java/org/identifiers/cloud/libapi/models/resolver/Recommendation.java
+++ src/main/java/org/identifiers/cloud/libapi/models/resolver/Recommendation.java
@@ -2,6 +2,8 @@ package org.identifiers.cloud.libapi.models.resolver;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import java.io.Serializable;
+
 /**
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * Project: libapi
@@ -10,7 +12,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
  * ---
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class Recommendation {
+public class Recommendation implements Serializable {
     // This models a recommendation attached to a particular resource in the response from the Resolver Web Service
     private int recommendationIndex = 0;
     private String recommendationExplanation = "--- default explanation ---";
