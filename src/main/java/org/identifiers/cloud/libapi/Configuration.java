76e2b0b151766828d93731d97720fd66f2029015
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-07 04:13:41 +0000

    put it in the configuration file

--- src/main/java/org/identifiers/cloud/libapi/Configuration.java
+++ src/main/java/org/identifiers/cloud/libapi/Configuration.java
@@ -1,5 +1,11 @@
 package org.identifiers.cloud.libapi;
 
+import org.identifiers.cloud.libapi.models.RestTemplateErrorHandlerLogError;
+import org.springframework.retry.backoff.FixedBackOffPolicy;
+import org.springframework.retry.policy.SimpleRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
+import org.springframework.web.client.ResponseErrorHandler;
+
 /**
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * Project: libapi
@@ -8,4 +14,25 @@ package org.identifiers.cloud.libapi;
  * ---
  */
 public class Configuration {
+    public static final int WS_REQUEST_RETRY_MAX_ATTEMPTS = 12;
+    public static final int WS_REQUEST_RETRY_BACK_OFF_PERIOD = 1500; // 1.5 seconds
+
+    public static RetryTemplate retryTemplate() {
+        RetryTemplate retryTemplate = new RetryTemplate();
+        SimpleRetryPolicy retryPolicy = new SimpleRetryPolicy();
+        retryPolicy.setMaxAttempts(WS_REQUEST_RETRY_MAX_ATTEMPTS);
+
+        FixedBackOffPolicy backOffPolicy = new FixedBackOffPolicy();
+        backOffPolicy.setBackOffPeriod(WS_REQUEST_RETRY_BACK_OFF_PERIOD);
+
+        retryTemplate = new RetryTemplate();
+        retryTemplate.setRetryPolicy(retryPolicy);
+        retryTemplate.setBackOffPolicy(backOffPolicy);
+        return retryTemplate;
+    }
+
+    public static ResponseErrorHandler responseErrorHandler() {
+        return new RestTemplateErrorHandlerLogError();
+    }
+
 }
