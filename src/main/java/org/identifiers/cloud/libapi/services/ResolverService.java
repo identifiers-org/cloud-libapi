bf6c76d6c400f2b3c36ea43be8e0a275d7fd3ee9
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-07 10:05:06 +0000

    resolver service client, placeholder

---
+++ src/main/java/org/identifiers/cloud/libapi/services/ResolverService.java
@@ -0,0 +1,11 @@
+package org.identifiers.cloud.libapi.services;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: libapi
+ * Package: org.identifiers.cloud.libapi.services
+ * Timestamp: 2018-03-07 10:04
+ * ---
+ */
+public class ResolverService {
+}
