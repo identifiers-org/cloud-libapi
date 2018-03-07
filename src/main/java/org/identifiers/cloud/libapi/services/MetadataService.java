bd2d40bd36bcfc8b29c42dd1b88ff5508cc35834
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-07 13:42:30 +0000

    metadata service wrapper, placeholder

---
+++ src/main/java/org/identifiers/cloud/libapi/services/MetadataService.java
@@ -0,0 +1,11 @@
+package org.identifiers.cloud.libapi.services;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: libapi
+ * Package: org.identifiers.cloud.libapi.services
+ * Timestamp: 2018-03-07 13:42
+ * ---
+ */
+public class MetadataService {
+}
