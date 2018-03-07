a3bc12b8c97d93bfaa72afa8aba5f1be312d9276
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-07 06:12:11 +0000

    test class

---
+++ src/main/java/org/identifiers/cloud/libapi/models/resourcerecommender/TestClass.java
@@ -0,0 +1,11 @@
+package org.identifiers.cloud.libapi.models.resourcerecommender;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: libapi
+ * Package: org.identifiers.cloud.libapi.models.resourcerecommender
+ * Timestamp: 2018-03-07 6:11
+ * ---
+ */
+public class TestClass {
+}
