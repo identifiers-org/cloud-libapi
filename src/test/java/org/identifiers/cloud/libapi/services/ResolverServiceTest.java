ec921867de93a4738cbfb09dd25dc445d0369a8e
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-07 10:38:13 +0000

    unit test for resolver client, TODO

---
+++ src/test/java/org/identifiers/cloud/libapi/services/ResolverServiceTest.java
@@ -0,0 +1,25 @@
+package org.identifiers.cloud.libapi.services;
+
+import org.junit.Test;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: libapi
+ * Package: org.identifiers.cloud.libapi.services
+ * Timestamp: 2018-03-07 10:35
+ * ---
+ */
+public class ResolverServiceTest {
+
+    // Again, this unit tests are a simple way of human validation of the client, as there is no test data at the
+    // current iteration of this library
+    @Test
+    public void requestCompactIdResolution() {
+        // TODO
+    }
+
+    @Test
+    public void requestCompactIdResolutionWithSelector() {
+        // TODO
+    }
+}
--- src/test/java/org/identifiers/cloud/libapi/services/ResourceRecommenderServiceTest.java
+++ src/test/java/org/identifiers/cloud/libapi/services/ResourceRecommenderServiceTest.java
@@ -29,7 +29,6 @@ public class ResourceRecommenderServiceTest {
 
     @Test
     public void requestRecommendations() {
-        // TODO
         // There is no testing data for this iteration of the library, so this unit test is design as a simple API
         // compliance test, where we check that the client can effectively connect the web service and pull a response
         // back from it
