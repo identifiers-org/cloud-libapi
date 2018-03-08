c51a2799c9e56598a72a6721935f7f72f559f8e9
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-08 11:31:44 +0000

    registry service wrapper, placeholder

---
+++ src/main/java/org/identifiers/cloud/libapi/services/RegistryService.java
@@ -0,0 +1,11 @@
+package org.identifiers.cloud.libapi.services;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: libapi
+ * Package: org.identifiers.cloud.libapi.services
+ * Timestamp: 2018-03-08 11:31
+ * ---
+ */
+public class RegistryService {
+}
