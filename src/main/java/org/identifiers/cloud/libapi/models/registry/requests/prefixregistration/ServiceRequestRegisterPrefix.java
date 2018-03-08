865a5da325df7526d1e5e08b8a9028873b0f85ac
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-08 11:29:33 +0000

    prefix registration request

---
+++ src/main/java/org/identifiers/cloud/libapi/models/registry/requests/prefixregistration/ServiceRequestRegisterPrefix.java
@@ -0,0 +1,13 @@
+package org.identifiers.cloud.libapi.models.registry.requests.prefixregistration;
+
+import org.identifiers.cloud.libapi.models.ServiceRequest;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: libapi
+ * Package: org.identifiers.cloud.libapi.models.registry.requests.prefixregistration
+ * Timestamp: 2018-03-08 5:44
+ * ---
+ */
+public class ServiceRequestRegisterPrefix extends ServiceRequest<ServiceRequestRegisterPrefixPayload> {
+}
