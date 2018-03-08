7f2e4a34c4b3150349d9e7499f1ce3a970b1c134
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-08 11:29:54 +0000

    validation request

---
+++ src/main/java/org/identifiers/cloud/libapi/models/registry/requests/validation/ServiceRequestValidate.java
@@ -0,0 +1,14 @@
+package org.identifiers.cloud.libapi.models.registry.requests.validation;
+
+import org.identifiers.cloud.libapi.models.ServiceRequest;
+import org.identifiers.cloud.libapi.models.registry.requests.prefixregistration.ServiceRequestRegisterPrefixPayload;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: libapi
+ * Package: org.identifiers.cloud.libapi.models.registry.requests.validation
+ * Timestamp: 2018-03-08 6:11
+ * ---
+ */
+public class ServiceRequestValidate extends ServiceRequest<ServiceRequestRegisterPrefixPayload> {
+}
