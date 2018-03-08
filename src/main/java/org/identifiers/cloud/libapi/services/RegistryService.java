890bdcc73b3cdc3bc8248a1189147f05263e8dc5
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-08 11:44:00 +0000

    default responses

--- src/main/java/org/identifiers/cloud/libapi/services/RegistryService.java
+++ src/main/java/org/identifiers/cloud/libapi/services/RegistryService.java
@@ -1,8 +1,13 @@
 package org.identifiers.cloud.libapi.services;
 
 import org.identifiers.cloud.libapi.Configuration;
+import org.identifiers.cloud.libapi.models.ServiceResponse;
+import org.identifiers.cloud.libapi.models.registry.responses.prefixregistration.ServiceResponseRegisterPrefix;
+import org.identifiers.cloud.libapi.models.registry.responses.prefixregistration.ServiceResponseRegisterPrefixPayload;
+import org.identifiers.cloud.libapi.models.registry.responses.validation.ServiceResponseValidateRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.retry.support.RetryTemplate;
 
 /**
@@ -21,5 +26,23 @@ public class RegistryService {
 
     public RegistryService(String host, String port) {
         serviceApiBaseline = String.format("http://%s:%s", host, port);
+    }
+
+    private <T> void initDefaultResponse(ServiceResponse<T> response, T payload) {
+        response.setApiVersion(apiVersion)
+                .setHttpStatus(HttpStatus.OK);
+        response.setPayload(payload);
+    }
+
+    private ServiceResponseValidateRequest createDefaultResponseValidationRequest() {
+        ServiceResponseValidateRequest response = new ServiceResponseValidateRequest();
+        initDefaultResponse(response, new ServiceResponseRegisterPrefixPayload());
+        return response;
+    }
+
+    private ServiceResponseRegisterPrefix createDefaultResponseRegisterPrefixRequest() {
+        ServiceResponseRegisterPrefix response = new ServiceResponseRegisterPrefix();
+        initDefaultResponse(response, new ServiceResponseRegisterPrefixPayload());
+        return response;
     }
 }
