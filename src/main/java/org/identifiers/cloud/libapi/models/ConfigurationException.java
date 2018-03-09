a28e7e35fd9fb61d1eb0eea9f07684c8d5ef91b1
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-09 11:20:10 +0000

    configuration exception

---
+++ src/main/java/org/identifiers/cloud/libapi/models/ConfigurationException.java
@@ -0,0 +1,14 @@
+package org.identifiers.cloud.libapi.models;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: libapi
+ * Package: org.identifiers.cloud.libapi.models
+ * Timestamp: 2018-03-09 11:19
+ * ---
+ */
+public class ConfigurationException extends RuntimeException {
+    public ConfigurationException(String message) {
+        super(message);
+    }
+}
