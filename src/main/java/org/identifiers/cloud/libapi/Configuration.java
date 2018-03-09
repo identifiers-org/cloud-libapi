f2d95fa1bc3433fd9d828a92976cf76ddb46ab03
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-09 10:38:43 +0000

    cloud deployment keys

--- src/main/java/org/identifiers/cloud/libapi/Configuration.java
+++ src/main/java/org/identifiers/cloud/libapi/Configuration.java
@@ -17,8 +17,17 @@ public class Configuration {
     public static final int WS_REQUEST_RETRY_MAX_ATTEMPTS = 12;
     public static final int WS_REQUEST_RETRY_BACK_OFF_PERIOD = 1500; // 1.5 seconds
     public enum InfrastructureDeploymentSelector {
+        AWS("aws", "Amazon Web Services deployment"),
+        GCLOUD("gcloud", "Google Cloud deployment"),
+        AZURE("azure", "Microsoft Azure deployment");
+
         private String key;
         private String description;
+
+        InfrastructureDeploymentSelector(String key, String description) {
+            this.key = key;
+            this.description = description;
+        }
     }
 
     public static RetryTemplate retryTemplate() {
