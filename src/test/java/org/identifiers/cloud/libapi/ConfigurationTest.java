c744fbf1e2b7d7d80bcb39177073b6fdf19c855d
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-09 12:20:38 +0000

    unit test for configuration

---
+++ src/test/java/org/identifiers/cloud/libapi/ConfigurationTest.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.libapi;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: libapi
+ * Package: org.identifiers.cloud.libapi
+ * Timestamp: 2018-03-09 12:20
+ * ---
+ */
+public class ConfigurationTest {
+
+}
