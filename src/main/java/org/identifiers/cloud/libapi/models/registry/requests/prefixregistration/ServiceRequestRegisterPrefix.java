017b121bed51152c19941bce527f25a7ed2dbdae
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-13 13:30:04 +0000

    documenting classes and methods for Maven Central publication

--- src/main/java/org/identifiers/cloud/libapi/models/registry/requests/prefixregistration/ServiceRequestRegisterPrefix.java
+++ src/main/java/org/identifiers/cloud/libapi/models/registry/requests/prefixregistration/ServiceRequestRegisterPrefix.java
@@ -8,6 +8,9 @@ import org.identifiers.cloud.libapi.models.ServiceRequest;
  * Package: org.identifiers.cloud.libapi.models.registry.requests.prefixregistration
  * Timestamp: 2018-03-08 5:44
  * ---
+ *
+ * This class models a service request, for registering a new prefix, to the registry service, by specializing the
+ * ServiceRequest class with the right payload.
  */
 public class ServiceRequestRegisterPrefix extends ServiceRequest<ServiceRequestRegisterPrefixPayload> {
 }
