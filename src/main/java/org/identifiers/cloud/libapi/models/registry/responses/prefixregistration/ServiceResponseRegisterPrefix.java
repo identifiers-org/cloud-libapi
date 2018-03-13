2b94a1a0eeeed6f45b55637a93de953ba652049c
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-13 13:40:27 +0000

    documenting classes and methods for Maven Central publication

--- src/main/java/org/identifiers/cloud/libapi/models/registry/responses/prefixregistration/ServiceResponseRegisterPrefix.java
+++ src/main/java/org/identifiers/cloud/libapi/models/registry/responses/prefixregistration/ServiceResponseRegisterPrefix.java
@@ -8,6 +8,9 @@ import org.identifiers.cloud.libapi.models.ServiceResponse;
  * Package: org.identifiers.cloud.libapi.models.registry.responses.prefixregistration
  * Timestamp: 2018-01-30 17:11
  * ---
+ *
+ * This class models a response to a prefix registration request, from the registration service, by specializing the
+ * Service Response class with the right payload.
  */
 public class ServiceResponseRegisterPrefix extends ServiceResponse<ServiceResponseRegisterPrefixPayload> {
 }
