d5f3b254ade13177f203c9c02b9a9c41ce746597
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-13 13:00:42 +0000

    documenting classes and methods for Maven Central publication

--- src/main/java/org/identifiers/cloud/libapi/models/metadata/ResponseFetchMetadataForUrlPayload.java
+++ src/main/java/org/identifiers/cloud/libapi/models/metadata/ResponseFetchMetadataForUrlPayload.java
@@ -6,6 +6,9 @@ package org.identifiers.cloud.libapi.models.metadata;
  * Package: org.identifiers.cloud.libapi.models.metadata
  * Timestamp: 2018-03-07 13:18
  * ---
+ *
+ * This class models the payload that is part of the metadata service response to a request for fetching metadata for a
+ * given URL.
  */
 public class ResponseFetchMetadataForUrlPayload extends ResponseFetchMetadataPayload {
 }
