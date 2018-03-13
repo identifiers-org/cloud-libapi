1a4c7f892bb06a7917ef892a30d44b72f79cd4dd
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-13 16:26:18 +0000

    registry service, documentation

--- src/main/java/org/identifiers/cloud/libapi/services/RegistryService.java
+++ src/main/java/org/identifiers/cloud/libapi/services/RegistryService.java
@@ -267,6 +267,15 @@ public class RegistryService {
         return requestValidation(String.format("%s/%s", serviceApiBaseline, "validateRegisterPrefixName"), payload);
     }
 
+    /**
+     * Registry Service API: validate 'description' field for a prefix registration request.
+     *
+     * This will not validate the whole prefix registration request, only the 'description' field of the request.
+     * @param payload a prefix registration payload that contains a value for the field that is being validated.
+     * @return validation request response for the prefix registration field that is being validated, or a guaranteed
+     * default response, for this context, where HTTP Status code and error message fields have information on what
+     * could have happened.
+     */
     public ServiceResponseValidateRequest requestValidationDescription(ServiceRequestRegisterPrefixPayload payload) {
         return requestValidation(String.format("%s/%s", serviceApiBaseline, "validateRegisterPrefixDescription"), payload);
     }
