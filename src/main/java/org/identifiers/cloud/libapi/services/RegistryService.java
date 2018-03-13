b322298648e9e1b777059adf270aa85cf7981a27
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-13 16:27:36 +0000

    registry service, documentation

--- src/main/java/org/identifiers/cloud/libapi/services/RegistryService.java
+++ src/main/java/org/identifiers/cloud/libapi/services/RegistryService.java
@@ -319,6 +319,15 @@ public class RegistryService {
         return requestValidation(String.format("%s/%s", serviceApiBaseline, "validateRegisterPrefixPreferredPrefix"), payload);
     }
 
+    /**
+     * Registry Service API: validate 'resource access rule' field for a prefix registration request.
+     *
+     * This will not validate the whole prefix registration request, only the 'resource access rule' field of the request.
+     * @param payload a prefix registration payload that contains a value for the field that is being validated.
+     * @return validation request response for the prefix registration field that is being validated, or a guaranteed
+     * default response, for this context, where HTTP Status code and error message fields have information on what
+     * could have happened.
+     */
     public ServiceResponseValidateRequest requestValidationResourceAccessRule(ServiceRequestRegisterPrefixPayload payload) {
         return requestValidation(String.format("%s/%s", serviceApiBaseline, "validateRegisterPrefixResourceAccessRule"), payload);
     }
