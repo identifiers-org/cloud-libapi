74c03ea25dabeb28b599635447f09666d60e407d
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-13 16:26:34 +0000

    registry service, documentation

--- src/main/java/org/identifiers/cloud/libapi/services/RegistryService.java
+++ src/main/java/org/identifiers/cloud/libapi/services/RegistryService.java
@@ -280,6 +280,15 @@ public class RegistryService {
         return requestValidation(String.format("%s/%s", serviceApiBaseline, "validateRegisterPrefixDescription"), payload);
     }
 
+    /**
+     * Registry Service API: validate 'home page' field for a prefix registration request.
+     *
+     * This will not validate the whole prefix registration request, only the 'home page' field of the request.
+     * @param payload a prefix registration payload that contains a value for the field that is being validated.
+     * @return validation request response for the prefix registration field that is being validated, or a guaranteed
+     * default response, for this context, where HTTP Status code and error message fields have information on what
+     * could have happened.
+     */
     public ServiceResponseValidateRequest requestValidationHomePage(ServiceRequestRegisterPrefixPayload payload) {
         return requestValidation(String.format("%s/%s", serviceApiBaseline, "validateRegisterPrefixHomePage"), payload);
     }
