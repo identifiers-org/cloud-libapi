e3a62390e7e1dcd28f32ba7f4e252abbadec8ccc
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-13 14:13:25 +0000

    documenting classes and methods for Maven Central publication

--- src/main/java/org/identifiers/cloud/libapi/models/ConfigurationException.java
+++ src/main/java/org/identifiers/cloud/libapi/models/ConfigurationException.java
@@ -6,6 +6,8 @@ package org.identifiers.cloud.libapi.models;
  * Package: org.identifiers.cloud.libapi.models
  * Timestamp: 2018-03-09 11:19
  * ---
+ *
+ * Used by the Configuration class(es) for notifying of possible errors.
  */
 public class ConfigurationException extends RuntimeException {
     public ConfigurationException(String message) {
