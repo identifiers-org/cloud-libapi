09350b1b1045ce9fc7cd07a6f368fbbdfb5bbb97
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-13 13:38:34 +0000

    documenting classes and methods for Maven Central publication

--- src/main/java/org/identifiers/cloud/libapi/models/registry/requests/validation/ServiceRequestValidate.java
+++ src/main/java/org/identifiers/cloud/libapi/models/registry/requests/validation/ServiceRequestValidate.java
@@ -9,6 +9,9 @@ import org.identifiers.cloud.libapi.models.registry.requests.prefixregistration.
  * Package: org.identifiers.cloud.libapi.models.registry.requests.validation
  * Timestamp: 2018-03-08 6:11
  * ---
+ *
+ * This class models a validation request, to the registry service, by specializing the ServiceRequest class with the
+ * right payload.
  */
 public class ServiceRequestValidate extends ServiceRequest<ServiceRequestRegisterPrefixPayload> {
 }
