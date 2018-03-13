8c7864053d44c589380de3e3650b3865767a83e0
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-13 13:01:30 +0000

    documenting classes and methods for Maven Central publication

--- src/main/java/org/identifiers/cloud/libapi/models/metadata/ResponseFetchMetadataPayload.java
+++ src/main/java/org/identifiers/cloud/libapi/models/metadata/ResponseFetchMetadataPayload.java
@@ -8,6 +8,9 @@ import java.io.Serializable;
  * Package: org.identifiers.cloud.libapi.models.metadata
  * Timestamp: 2018-03-07 10:59
  * ---
+ *
+ * This class models the payload that is part of the metadata service response, when requesting metadata for a given
+ * Compact ID
  */
 public class ResponseFetchMetadataPayload implements Serializable {
     Object metadata;
