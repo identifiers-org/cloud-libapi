98f51a7863c7b6cb6b2e5d9e95702fcc7b9c16ac
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-13 13:05:30 +0000

    documenting classes and methods for Maven Central publication

--- src/main/java/org/identifiers/cloud/libapi/models/metadata/ServiceRequestFetchMetadataForUrl.java
+++ src/main/java/org/identifiers/cloud/libapi/models/metadata/ServiceRequestFetchMetadataForUrl.java
@@ -8,6 +8,9 @@ import org.identifiers.cloud.libapi.models.ServiceRequest;
  * Package: org.identifiers.cloud.libapi.models.metadata
  * Timestamp: 2018-03-07 13:17
  * ---
+ *
+ * This class models a metadata request, to the metadata service, for a given URL, by specializing the ServiceRequest
+ * class with the right payload
  */
 public class ServiceRequestFetchMetadataForUrl extends ServiceRequest<RequestFetchMetadataForUrlPayload> {
 }
