be9209290f3e37b2c0664b9ee28bd609ffb90ed9
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-13 13:06:26 +0000

    documenting classes and methods for Maven Central publication

--- src/main/java/org/identifiers/cloud/libapi/models/metadata/ServiceResponseFetchMetadata.java
+++ src/main/java/org/identifiers/cloud/libapi/models/metadata/ServiceResponseFetchMetadata.java
@@ -8,6 +8,9 @@ import org.identifiers.cloud.libapi.models.ServiceResponse;
  * Package: org.identifiers.cloud.libapi.models.metadata
  * Timestamp: 2018-03-07 11:01
  * ---
+ *
+ * This class models a response from the metadata service to a metadata request for a Compact ID, by specializing the
+ * ServiceResponse class with the right payload.
  */
 public class ServiceResponseFetchMetadata extends ServiceResponse<ResponseFetchMetadataPayload> {
 }
