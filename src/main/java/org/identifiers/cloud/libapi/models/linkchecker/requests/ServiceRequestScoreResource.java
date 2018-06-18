a115777fc448ee19988d3e65df41a9dd99f04901
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-06-18 09:39:16 +0100

    linkchecker models, ServiceRequestScoreResource

---
+++ src/main/java/org/identifiers/cloud/libapi/models/linkchecker/requests/ServiceRequestScoreResource.java
@@ -0,0 +1,14 @@
+package org.identifiers.cloud.libapi.models.linkchecker.requests;
+
+import org.identifiers.cloud.libapi.models.ServiceRequest;
+
+/**
+ * Project: libapi
+ * Package: org.identifiers.cloud.libapi.models.linkchecker.requests
+ * Timestamp: 2018-06-18 9:38
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class ServiceRequestScoreResource extends ServiceRequest<ScoringRequestWithIdPayload> {
+}
