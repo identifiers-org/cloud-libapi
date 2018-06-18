484fd2941f7ae6ff0ce1466e95a85bae57ad6d4f
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-06-18 09:44:47 +0100

    linkchecker models, responses, ServiceResponseScoringRequestPayload

---
+++ src/main/java/org/identifiers/cloud/libapi/models/linkchecker/responses/ServiceResponseScoringRequestPayload.java
@@ -0,0 +1,28 @@
+package org.identifiers.cloud.libapi.models.linkchecker.responses;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.io.Serializable;
+
+/**
+ * Project: libapi
+ * Package: org.identifiers.cloud.libapi.models.linkchecker.responses
+ * Timestamp: 2018-06-18 9:44
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ServiceResponseScoringRequestPayload implements Serializable {
+    // Default scoring
+    public int score = 50;
+
+    public int getScore() {
+        return score;
+    }
+
+    public ServiceResponseScoringRequestPayload setScore(int score) {
+        this.score = score;
+        return this;
+    }
+}
