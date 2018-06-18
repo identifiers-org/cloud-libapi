ef14843a6c4553f0d8268c69385edb7edeb0cd3b
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-06-18 12:09:38 +0100

    link checker service client unit test, placeholder

---
+++ src/test/java/org/identifiers/cloud/libapi/services/LinkCheckerServiceTest.java
@@ -0,0 +1,16 @@
+package org.identifiers.cloud.libapi.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Project: libapi
+ * Package: org.identifiers.cloud.libapi.services
+ * Timestamp: 2018-06-18 12:07
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class LinkCheckerServiceTest {
+    private static Logger logger = LoggerFactory.getLogger(LinkCheckerServiceTest.class);
+}
