7a31a25a3ea1271042ec92aad8306770adb2f48f
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-06-18 09:48:14 +0100

    linkchecker service wrapper, placeholder

---
+++ src/main/java/org/identifiers/cloud/libapi/services/LinkCheckerService.java
@@ -0,0 +1,15 @@
+package org.identifiers.cloud.libapi.services;
+
+/**
+ * Project: libapi
+ * Package: org.identifiers.cloud.libapi.services
+ * Timestamp: 2018-06-18 9:47
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ *
+ * This class implements a client for the Link Checker Service.
+ */
+public class LinkCheckerService {
+    // TODO
+}
