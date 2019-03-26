c8a540b2bc1269c50073412d7260462329de4945
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-03-26 10:50:44 +0000

    mir id controller service client, placeholder

---
+++ src/main/java/org/identifiers/cloud/libapi/services/MirIdControllerService.java
@@ -0,0 +1,12 @@
+package org.identifiers.cloud.libapi.services;
+
+/**
+ * Project: cloud-libapi
+ * Package: org.identifiers.cloud.libapi.services
+ * Timestamp: 2019-03-26 10:49
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class MirIdControllerService {
+}
