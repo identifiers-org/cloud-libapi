ca45d160f79ab938d9070d3f1f550583b57ac031
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-04-03 10:12:37 +0100

    resolver client, data model, institution

---
+++ src/main/java/org/identifiers/cloud/libapi/models/resolver/Institution.java
@@ -0,0 +1,32 @@
+package org.identifiers.cloud.libapi.models.resolver;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+/**
+ * Project: cloud-libapi
+ * Package: org.identifiers.cloud.libapi.models.resolver
+ * Timestamp: 2019-04-03 10:10
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Accessors(chain = true)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Institution {
+    // identifiers.org internal ID for this institution
+    private long id;
+    private String name;
+    private String homeUrl;
+    private String description;
+    // Geographical location for this institution
+    private Location location;
+}
