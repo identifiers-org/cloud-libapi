6702744ce6c193577f856f9bc57898b1f12bdf78
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-04-03 10:12:27 +0100

    resolver client, data model location

---
+++ src/main/java/org/identifiers/cloud/libapi/models/resolver/Location.java
@@ -0,0 +1,25 @@
+package org.identifiers.cloud.libapi.models.resolver;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+/**
+ * Project: cloud-libapi
+ * Package: org.identifiers.cloud.libapi.models.resolver
+ * Timestamp: 2019-04-03 10:11
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Accessors(chain = true)
+public class Location {
+    private String countryCode;
+    private String countryName;
+}
