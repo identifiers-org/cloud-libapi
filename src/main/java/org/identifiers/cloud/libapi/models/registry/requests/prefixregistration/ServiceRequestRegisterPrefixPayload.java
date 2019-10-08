efe53b93d461c6579f37b37bc61b07e7628784ca
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-10-08 15:46:06 +0100

    ServiceRequestRegisterPrefixPayload refactoring

--- src/main/java/org/identifiers/cloud/libapi/models/registry/requests/prefixregistration/ServiceRequestRegisterPrefixPayload.java
+++ src/main/java/org/identifiers/cloud/libapi/models/registry/requests/prefixregistration/ServiceRequestRegisterPrefixPayload.java
@@ -1,5 +1,11 @@
 package org.identifiers.cloud.libapi.models.registry.requests.prefixregistration;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 import org.identifiers.cloud.libapi.models.registry.Requester;
 
 import java.io.Serializable;
@@ -16,115 +22,67 @@ import java.io.Serializable;
  *
  * It's a basic POJO with self explanatory fields.
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Accessors(chain = true)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ServiceRequestRegisterPrefixPayload implements Serializable {
+    // Model from HQ Registry service
+    // Name for the prefix being registered, ported from the original identifiers.org form at https://identifiers.org/request/prefix
     private String name;
+
+    // This is a description for the namespace being registered, ported from the original identifiers.org form at https://identifiers.org/request/prefix
     private String description;
-    private String homePage;
-    private String organization;
-    private String preferredPrefix;
-    private String resourceAccessRule;
-    private String exampleIdentifier;
-    private String regexPattern;
-    private String[] references;
+
+    // Home URL for a first provider of this namespace being registered
+    private String providerHomeUrl;
+
+    // Name for the provider being registered along this prefix
+    private String providerName;
+
+    // Description for the provider being registered along this prefix
+    private String providerDescription;
+
+    // Location Associated with the provider being registered along this prefix
+    private String providerLocation;
+
+    // Unique identifier for this provider within the namespace for provider selection when resolving compact identifiers
+    // belonging to the namespace being registered
+    private String providerCode;
+
+    // This is the name of the institution that owns the resource that's being registered as first provider for this namespace
+    private String institutionName;
+
+    // Home URL for the institution (this is a new requirement)
+    private String institutionHomeUrl;
+
+    // A description related to the institution that owns the resource that's being registered as first provider for this namespace
+    private String institutionDescription;
+
+    // Location of the institution that owns the resource that's being registered as first provider for this namespace
+    private String institutionLocation;
+
+    // ROR ID for this institution
+    private String institutionRorId;
+
+    // Originally called 'preferredPrefix', ported from the original identifiers.org form at https://identifiers.org/request/prefix
+    private String requestedPrefix;
+
+    // Originally called 'resourceAccessRule', ported from the original identifiers.org form at https://identifiers.org/request/prefix
+    private String providerUrlPattern;
+
+    // Originally called 'exampleIdentifier', ported from the original identifiers.org form at https://identifiers.org/request/prefix
+    private String sampleId;
+
+    // Originally called 'regexPattern', ported from the original identifiers.org form at https://identifiers.org/request/prefix
+    private String idRegexPattern;
+
+    private String[] supportingReferences;
     private String additionalInformation;
     private Requester requester;
 
-    public String getName() {
-        return name;
-    }
-
-    public ServiceRequestRegisterPrefixPayload setName(String name) {
-        this.name = name;
-        return this;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public ServiceRequestRegisterPrefixPayload setDescription(String description) {
-        this.description = description;
-        return this;
-    }
-
-    public String getHomePage() {
-        return homePage;
-    }
-
-    public ServiceRequestRegisterPrefixPayload setHomePage(String homePage) {
-        this.homePage = homePage;
-        return this;
-    }
-
-    public String getOrganization() {
-        return organization;
-    }
-
-    public ServiceRequestRegisterPrefixPayload setOrganization(String organization) {
-        this.organization = organization;
-        return this;
-    }
-
-    public String getPreferredPrefix() {
-        return preferredPrefix;
-    }
-
-    public ServiceRequestRegisterPrefixPayload setPreferredPrefix(String preferredPrefix) {
-        this.preferredPrefix = preferredPrefix;
-        return this;
-    }
-
-    public String getResourceAccessRule() {
-        return resourceAccessRule;
-    }
-
-    public ServiceRequestRegisterPrefixPayload setResourceAccessRule(String resourceAccessRule) {
-        this.resourceAccessRule = resourceAccessRule;
-        return this;
-    }
-
-    public String getExampleIdentifier() {
-        return exampleIdentifier;
-    }
-
-    public ServiceRequestRegisterPrefixPayload setExampleIdentifier(String exampleIdentifier) {
-        this.exampleIdentifier = exampleIdentifier;
-        return this;
-    }
-
-    public String getRegexPattern() {
-        return regexPattern;
-    }
-
-    public ServiceRequestRegisterPrefixPayload setRegexPattern(String regexPattern) {
-        this.regexPattern = regexPattern;
-        return this;
-    }
-
-    public String[] getReferences() {
-        return references;
-    }
-
-    public ServiceRequestRegisterPrefixPayload setReferences(String[] references) {
-        this.references = references;
-        return this;
-    }
-
-    public String getAdditionalInformation() {
-        return additionalInformation;
-    }
-
-    public ServiceRequestRegisterPrefixPayload setAdditionalInformation(String additionalInformation) {
-        this.additionalInformation = additionalInformation;
-        return this;
-    }
-
-    public Requester getRequester() {
-        return requester;
-    }
-
-    public ServiceRequestRegisterPrefixPayload setRequester(Requester requester) {
-        this.requester = requester;
-        return this;
-    }
+    // Flag whether this namespace has LUIs with namespace embedded, which is a special case, as they're allowed to omit their namespace when hitting the resolver.
+    private boolean namespaceEmbeddedInLui = false;
 }
