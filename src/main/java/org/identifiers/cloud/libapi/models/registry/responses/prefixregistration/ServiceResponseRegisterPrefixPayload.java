5516967996453fc6456ebd26e235dbe4d4e97f61
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-10-08 15:52:00 +0100

    ServiceResponseRegisterPrefixPayload refactoring

--- src/main/java/org/identifiers/cloud/libapi/models/registry/responses/prefixregistration/ServiceResponseRegisterPrefixPayload.java
+++ src/main/java/org/identifiers/cloud/libapi/models/registry/responses/prefixregistration/ServiceResponseRegisterPrefixPayload.java
@@ -1,6 +1,11 @@
 package org.identifiers.cloud.libapi.models.registry.responses.prefixregistration;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 
 import java.io.Serializable;
 
@@ -13,16 +18,16 @@ import java.io.Serializable;
  *
  * This class models the payload information within a prefix registration request response, from the registry service.
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Accessors(chain = true)
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ServiceResponseRegisterPrefixPayload implements Serializable {
+    // Comments on the prefix registration request
     private String comment = "No comments on your prefix registration request";
-
-    public String getComment() {
-        return comment;
-    }
-
-    public ServiceResponseRegisterPrefixPayload setComment(String comment) {
-        this.comment = comment;
-        return this;
-    }
+    // This is an ephemeral token whose lifecycle is tight to the lifecycle of the prefix registration requests, so,
+    // once the prefix registration has been solved, either rejected or accepted, this token is no longer valid.
+    private String token = "";
 }
