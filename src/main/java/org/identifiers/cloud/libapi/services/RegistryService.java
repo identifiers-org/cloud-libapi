f544db1192ed81474c6adc08dbecfa37f968d999
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-10-10 13:01:37 +0100

    documentation for validation of institution description

--- src/main/java/org/identifiers/cloud/libapi/services/RegistryService.java
+++ src/main/java/org/identifiers/cloud/libapi/services/RegistryService.java
@@ -404,6 +404,15 @@ public class RegistryService {
         return requestValidation(String.format("%s/%s", getServiceApiEndpointBaseline(), "validateInstitutionHomeUrl"), payload);
     }
 
+    /**
+     * Registry Service API: validate institution description.
+     *
+     * This will not validate the whole prefix registration request, only the given institution description.
+     * @param payload a prefix registration payload that contains a value for the field that is being validated.
+     * @return validation request response for the prefix registration field that is being validated, or a guaranteed
+     * default response, for this context, where HTTP Status code and error message fields have information on what
+     * could have happened.
+     */
     public ServiceResponseValidateRequest validateInstitutionDescription(ServiceRequestRegisterPrefixPayload payload) {
         return requestValidation(String.format("%s/%s", getServiceApiEndpointBaseline(), "validateInstitutionDescription"), payload);
     }
