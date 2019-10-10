cef1f619b53040fbfa34c40d05497c12c48a7257
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-10-10 12:57:30 +0100

    documentation for validation of provider url pattern

--- src/main/java/org/identifiers/cloud/libapi/services/RegistryService.java
+++ src/main/java/org/identifiers/cloud/libapi/services/RegistryService.java
@@ -365,6 +365,15 @@ public class RegistryService {
         return requestValidation(String.format("%s/%s", getServiceApiEndpointBaseline(), "validateProviderCode"), payload);
     }
 
+    /**
+     * Registry Service API: validate the given provider URL pattern.
+     *
+     * This will not validate the whole prefix registration request, only the given provider URL pattern.
+     * @param payload a prefix registration payload that contains a value for the field that is being validated.
+     * @return validation request response for the prefix registration field that is being validated, or a guaranteed
+     * default response, for this context, where HTTP Status code and error message fields have information on what
+     * could have happened.
+     */
     public ServiceResponseValidateRequest validateProviderUrlPattern(ServiceRequestRegisterPrefixPayload payload) {
         return requestValidation(String.format("%s/%s", getServiceApiEndpointBaseline(), "validateProviderUrlPattern"), payload);
     }
