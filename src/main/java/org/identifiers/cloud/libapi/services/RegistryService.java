2a2c2943ad21aa39aeaf0724da6ba2fb78c3f683
Manuel Bernal Llinares <mbdebian@gmail.com>
2019-10-10 13:16:23 +0100

    refactoring documentation

--- src/main/java/org/identifiers/cloud/libapi/services/RegistryService.java
+++ src/main/java/org/identifiers/cloud/libapi/services/RegistryService.java
@@ -442,7 +442,7 @@ public class RegistryService {
     public ServiceResponseValidateRequest validateRequestedPrefix(ServiceRequestRegisterPrefixPayload payload) {
         return requestValidation(String.format("%s/%s", getServiceApiEndpointBaseline(), "validateRequestedPrefix"), payload);
     }
-    
+
     /**
      * Registry Service API: validate the given sample ID
      *
@@ -459,7 +459,8 @@ public class RegistryService {
     /**
      * Registry Service API: validate the regular expression that represents LUIs in the requested namespace
      *
-     * This will not validate the whole prefix registration request, only the 'regex pattern' field of the request.
+     * This will not validate the whole prefix registration request, only the given regular expression that represents
+     * LUIs in the requested namespace.
      * @param payload a prefix registration payload that contains a value for the field that is being validated.
      * @return validation request response for the prefix registration field that is being validated, or a guaranteed
      * default response, for this context, where HTTP Status code and error message fields have information on what
@@ -508,6 +509,15 @@ public class RegistryService {
         return requestValidation(String.format("%s/%s", getServiceApiEndpointBaseline(), "validateRequester"), payload);
     }
 
+    /**
+     * Registry Service API: validate the requester name.
+     *
+     * This will not validate the whole prefix registration request, only the requester name.
+     * @param payload a prefix registration payload that contains a value for the field that is being validated.
+     * @return validation request response for the prefix registration field that is being validated, or a guaranteed
+     * default response, for this context, where HTTP Status code and error message fields have information on what
+     * could have happened.
+     */
     public ServiceResponseValidateRequest validateRequesterName(ServiceRequestRegisterPrefixPayload payload) {
         return requestValidation(String.format("%s/%s", getServiceApiEndpointBaseline(), "validateRequesterName"), payload);
     }
